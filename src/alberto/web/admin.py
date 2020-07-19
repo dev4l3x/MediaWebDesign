203b7037bc875774cc2013bb04c1f932dccdb6ed
alexsi <alexsi@ue.balidea.com>
2020-07-19 11:00:56 +0200

    Deleted all

--- src/alberto/web/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-# Register your models here.
-from web.models.idioma import Idioma
-
-admin.site.register(Idioma)
--- src/alberto/web/models/idioma.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class Idioma(models.Model):
-    nombre = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.nombre
