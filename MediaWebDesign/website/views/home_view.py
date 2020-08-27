28fd2f50d977ea85fc5c5f2521b6c8905cc18d17
alexsi <asiglesias478@gmail.com>
2020-08-27 18:21:58 +0200

    Soporte para imagenes

--- MediaWebDesign/website/views/home_view.py
+++ MediaWebDesign/website/views/home_view.py
@@ -1,14 +1,18 @@
 from django.http import HttpResponse
 from django.views import View
 from django.template import loader
-from website.models import Service, SocialNetwork
+from website.models import Service, SocialNetwork, Video, Image
 
 
 def index(request):
     template = loader.get_template('home.html')
     services = Service.objects.all()
     social_networks = SocialNetwork.objects.all()
+    videos = Video.objects.all()
+    photos = Image.objects.all()
     context = {
+        'photos': photos,
+        'videos': videos,
         'services': services,
         'networks': social_networks
     }
