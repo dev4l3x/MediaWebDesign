63b9b91797c1261a2b5f4d556c84cbf429d28b33
dev4l3x <asiglesias478@gmail.com>
2021-01-06 02:21:21 +0100

    Fixed visualization errors on mobile
    Changed animations that were causing visual errors while scrolling on mobile

--- MediaWebDesign/MediaWebDesign/settings.py
+++ MediaWebDesign/MediaWebDesign/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = str(config('APP_SECRET'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(config('DEBUG'))
 
-ALLOWED_HOSTS = ['192.168.8.107', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
